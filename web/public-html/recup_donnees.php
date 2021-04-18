ea0cb6a3a255df1b907e9113eaa91fee40127c4d
Quentins <HE201860@students.ephec.be>
2021-04-18 23:01:14 +0200

    update dockerfile
    changement image -> PHP
    modification site web

--- web/public-html/recup_donnees.php
+++ web/public-html/recup_donnees.php
@@ -1,4 +1,10 @@
 <?php
-reponses($_POST);
-echo reponses;
+if (isset($_POST['submit'])){
+  $nom = $_POST['nom'];
+  $prenom = $_POST['prenom'];
+  $sexe = $_POST['sexe'];
+  echo '<h3>infos</h3>';
+  echo 'Nom: ' . $nom . 'Prenom: ' . $prenom . 'Sexe: ' . $sexe;
+}
+echo 'not set'
 ?>
