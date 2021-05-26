c1db716624b4ca307b6831935b4d9751336b49f3
Mikael <he201775@students.ephec.be>
2021-05-26 13:38:24 +0200

    modification de l'intranet

--- web/intranet/index.php
+++ web/intranet/index.php
@@ -1,6 +1,5 @@
-
 <?php
-try {
+try { 
     $objetPdo = new PDO('mysql:host=192.168.10.2;dbname=woody;charset=utf8', 'root', 'root');
     echo "connect OK";
     $pdoStat = $objetPdo->prepare('SELECT * FROM clients');
@@ -14,27 +13,42 @@ try {
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Ceci est une page de test avec des balises PHP</title>
-    <meta charset="utf-8" />
-</head>
-<body>
-<h2>Page de test</h2>
-
-<p>
-    Cette page contient du code HTML avec des balises PHP.<br />
-    <?php echo "Ceci est du texte"; ?>
-    Voici quelques petits tests :
-</p>
-
-<ul>
-    <li style="color: blue;">Texte en bleu</li>
-    <li style="color: red;">Texte en rouge</li>
-    <li style="color: green;">Texte en vert</li>
-</ul>
-
-<?php
-    echo "Ceci est du texte";
-?>
-</body>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Woody Toys</title>
+  </head>
+  <body>
+    <h1>Intranet Woody Toys</h1>
+    <h2>Commandes:</h2>
+    <div id="Commandes">
+    <?php
+    foreach ($Commandes as $Commande) {
+        echo "<li>" . $Commande['nom'] . " " . $Commande['client'] . "</li>";
+      }
+    ?>
+    </div>
+    <h2>Devenir client</h2>
+    <form action="#" method="post">
+      <p>
+        <label for="nom">Votre Nom</label>
+        <input type="text" id="nom" name="nom" placeholder="Mathioux">
+      </p>
+      <p>
+        <label for="client">Votre prénom</label>
+        <input type="text" id="client" name="client" placeholder="Licorne">
+      </p>
+      <p>
+        <input type="submit" value="Enregistrer">
+      </p>
+    </form>
+    <?php
+      if (isset($_POST['nom'], $_POST['client'])){
+        $requete_insertion = $objetPdo -> prepare('INSERT INTO commandes(nom, client) VALUES (:n, :p)');
+        $requete_insertion -> execute(array(
+          'n' => $_POST["nom"],
+          'p' => $_POST["prenom"]
+        ));
+      }
+    ?>
+  </body>
 </html>
